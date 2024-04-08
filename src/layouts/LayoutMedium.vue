6e417d09d35dd66d65dff45da6c77b66091610e2
BeatrizVenturi <venturibeatriz329@gmail.com>
2024-04-08 11:39:37 -0300

    ajuste de layout para layout small e medium :)

--- src/layouts/LayoutMedium.vue
+++ src/layouts/LayoutMedium.vue
@@ -5,20 +5,68 @@ import MediumHeader from '@/components/teamplates/medium/MediumHeader.vue';
 </script>
 
 <template>
-    <header>
-        <medium-header />
-    </header>
+    <div id="layout-medium">
+        <header>
+            <medium-header />
+        </header>
 
-    <aside>
-        <medium-aside />
-    </aside>
+        <aside>
+            <medium-aside />
+        </aside>
 
-    <main>
-        <router-view />
-    </main>
+        <main>
+            <router-view />
+        </main>
 
-    <footer>
-        <medium-footer />
-    </footer>
-
+        <footer>
+            <medium-footer />
+        </footer>
+    </div>
 </template>
+<style scoped>
+#layout-medium {
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas:
+        'aside header'
+        'aside main'
+        'aside footer';
+    min-height: 100vh;
+}
+
+header {
+    display: flex;
+    widows: 100vw;
+    justify-content: flex-end;
+    padding: 1.5rem;
+    grid-area: header;
+    background-color: aqua;
+}
+
+aside {
+    border-right: #eeeeee 1px solid;
+    box-shadow: 0 0 10px 0 #eeeeee;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    grid-area: aside;
+}
+
+main {
+    min-height: 80%;
+    padding: 2rem;
+    grid-area: main;
+}
+
+footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3rem;
+    background-color: #eeeeee;
+    grid-area: footer;
+}
+</style>
